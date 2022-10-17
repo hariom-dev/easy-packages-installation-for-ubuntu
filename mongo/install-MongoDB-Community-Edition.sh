676f6ee8b2f9fa13998dbe0f02e4bec898915734
Hariom Dubey <hariomdubey.dev@gmail.com>
2022-10-17 16:00:41 +0530

    added docs

---
+++ mongo/install-MongoDB-Community-Edition.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Install MongoDB Community Edition
+
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+
+if [[ $(lsb_release -rs) == "20.04" ]]; then
+
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+elif [[$(lsb_release -rs) == "18.04"]]; then
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+elif [[$(lsb_release -rs) == "16.04"]]; then
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+else
+    echo "Can't install mongo DB cause os is compatible."
+fi
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-database hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+sudo service mongod start
+sudo service mongod status
